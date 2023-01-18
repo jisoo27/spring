7bfd6771715518712cbb494036f07730eb6b45a1
jisu <jisuuoo96@gmail.com>
2023-01-18 19:24:24 +0900

    :sparkles: itemEntity, itemRepository 구현

---
+++ item-service/src/main/java/hello/itemservice/domain/ItemRepository.java
@@ -0,0 +1,40 @@
+package hello.itemservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository // 컴포넌트 스캔에 대상이 된다.
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); // 실제는 HashMap 안됨
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values()); // 안전하게 감싼 것.
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
